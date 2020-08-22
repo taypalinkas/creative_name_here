1a311ce1fc659b74ce249f8e8e7337ba3fbbf7a7
Javier Ortiz <javierortiz@javiers-mbp.attlocal.net>
2020-08-22 11:14:23 -0700

    bubble chart

---
+++ candlestick/bubble.js
@@ -0,0 +1,96 @@
+function makeResponsive() {
+
+  // resize chart according to window size
+var svgArea = d3.select("body").select("svg");
+
+if (!svgArea.empty()) {
+  svgArea.remove();
+}
+
+  // svg params
+var svgHeight = window.innerHeight;
+var svgWidth = window.innerWidth;
+
+var margin = {
+  top: 50,
+  right: 100,
+  bottom: 50,
+  left: 20
+};
+
+var chartwidth = svgWidth - margin.left - margin.right;
+var chartheight = svgHeight - margin.top - margin.bottom;
+
+var svg = d3
+.select("body")
+.append("svg")
+.attr("width", svgWidth)
+.attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+.attr("transform", `translate(${margin.left - 20 } , ${margin.top })`);
+
+//Read the data
+d3.csv("condensed_dow_and_sentiment.csv", function(data) {
+
+  // Add X axis
+  var x = d3.scaleLinear()
+    .domain([-1, 1])
+    .range([ 100, (chartwidth) ]);
+  svg.append("g")
+  .attr("transform", `translate(${chartwidth / chartwidth}, ${(chartheight)+ 50})`)
+    .call(d3.axisBottom(x));
+
+  // Add Y axis
+  var y = d3.scaleLinear()
+    .domain([15, 2000])
+    .range([ chartheight, 0]);
+  svg.append("g")
+  .attr("transform", `translate(${(chartwidth/chartwidth) + 100}, ${(chartheight/chartheight)+ 50})`)
+    .call(d3.axisLeft(y));
+
+  // Add a scale for bubble size
+  var z = d3.scaleLinear()
+    .domain([-1, 1])
+    .range([ 2, 2]);
+
+  // Add dots
+  chartGroup.append('g')
+    .selectAll("dot")
+    .data(data)
+    .enter()
+    .append("circle")
+      .attr("cx", function (d) { return x(d.Vader_compound); } )
+      .attr("cy", function (d) { return y(d.Volatility); } )
+      .attr("r", function (d) { return z(d.Close); } )
+      .style("fill", "#69b3a2")
+      .style("opacity", "0.7")
+      .attr("stroke", "black")
+
+  chartGroup.append("text")
+      .attr("x", ((chartwidth + 100)/ 2))             
+      .attr("y", 0 - ((margin.top - 10 )/ 2))
+      .attr("text-anchor", "middle")  
+      .style("font-size", "16px") 
+      .attr("class", "axisText")
+      .text("Value vs Date Graph");
+  
+  chartGroup.append("text")
+      .attr("transform", `translate(${(chartwidth/2) + 50}, ${(chartheight) + 40})`)
+      .attr("class", "axisText")
+      .text("Year");
+
+  chartGroup.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", margin.left)
+      .attr("x", 0 - (chartheight / 2))
+      .attr("dy", "1em")
+      .attr("class", "axisText")
+      .text("Volatility");
+})
+}
+makeResponsive();
+
+// Event listener for window resize
+d3.select(window).on("resize", makeResponsive);
+
